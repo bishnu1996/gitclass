41a9ed4de4a5152257e346b195364a8cad3f8740
bishnu1996 <bishnumishra01996@gmail.com>
2022-03-02 20:18:45 +0530

    pattern.js

---
+++ pattern.js
@@ -0,0 +1,26 @@
+//1. pattern-1
+// for(i=5;i>0;i--){
+//     console.log("* * * * *");
+// }
+
+//2. pattern-1 type-2
+// let p="";
+// for(i=5;i>0;i--){
+//     for(j=5;j>0;j--){
+//         p+="* ";
+//     }
+//     p+="\n";
+// }
+// console.log(p);
+
+
+//2.pattern-2
+
+// for(i=1;i<=5;i++){
+//     console.log(i,i,i,i,i);
+// }
+
+//3. pattern-3
+for(i=1;i<=5;i++){
+    console.log("1 2 3 4 5");
+}
